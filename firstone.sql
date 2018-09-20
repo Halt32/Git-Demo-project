b7b2a47663436497cc5d6d6c4ec245a54cf0380e
Saikrishna Raavi <ravi.saikrishna487@gmail.com>
2018-09-20 14:46:10 -0400

    added a sql file

---
+++ firstone.sql
@@ -0,0 +1,14 @@
+create table students (
+	id integer auto_increment,
+    name text,
+    marks integer );
+
+INSERT into students ( name , marks)
+values ( 'ashely', 81 );
+INSERT into students ( name , marks)
+values ( 'samantha', 75);
+INSERT into students ( name , marks)
+values ('julia', 76);
+INSERT into students ( name , marks)
+values ('belvet', 84);
+
